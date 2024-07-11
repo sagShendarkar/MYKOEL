c0777c5f0949bf880d69f7c2e55b30a432d2c211
Arati-Dewarde <arati.dewarde@actin.co.in>
2024-07-11 10:45:29 +0530

    added auto mapper

--- MyKoel-Domain/Mapper/AutoMapperProfiles.cs
+++ MyKoel-Domain/Mapper/AutoMapperProfiles.cs
@@ -24,6 +24,7 @@ namespace API.Mapper
             CreateMap<Attachments, AddSectionTrnDto>();
             CreateMap<AttachmentDto, Attachments>();
             CreateMap<Attachments, AttachmentDto>();
+            CreateMap<AddSectionTrnDto, AddSectionTrnDto>();
 
         }
     }
