502f571324fa60bd91a90c7b39d5ed5eaae6b3cf
Arati-Dewarde <arati.dewarde@actin.co.in>
2024-07-22 11:54:04 +0530

    1th level api changes

--- MyKoel-Domain/Repositories/MenuHierarchyRepository.cs
+++ MyKoel-Domain/Repositories/MenuHierarchyRepository.cs
@@ -344,7 +344,7 @@ namespace MyKoel_Domain.Repositories
                                     //   join u in _context.UserMenuMap
                                     //   on menu.MenusId equals u.MenusId
                                       //where u.UserId == UserId && menu.IsActive==true
-                                      where MenuId > 0 ? menu.MenusId == MenuId :true
+                                      where (Level==1) ? menu.Level==Level : true  && (MenuId > 0 ? menu.MenusId == MenuId :true)
                                       group menu by new { menu.MenusId } into grouped
                                       select new MenuDataListDto
                                       {
